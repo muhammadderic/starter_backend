5676cfa290422cce091686f079d26101486f6032
muhammadderic <muhammad.donny.e@gmail.com>
2023-06-28 22:34:57 +0700

    Connect to the database

--- app.js
+++ app.js
@@ -11,10 +11,18 @@ dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Connect to the database
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("database is connected"))
+  .then(
+    // Listen
+    () => app.listen(process.env.PORT, () => {
+      console.log("server is listening on port:", process.env.PORT);
+    })
+  )
+  .catch(error => {
+    console.error({ message: error.message });
+  })
+
 // Routes
 app.use("/api/v1/todos", todoRoutes);
-
-// Listen
-app.listen(process.env.PORT, () => {
-  console.log("server is listening on port:", process.env.PORT);
-})
